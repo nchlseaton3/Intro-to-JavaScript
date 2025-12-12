65c62caac19928e16187e7ab881c25bdd08824c5
Nicholas <nchlseaton3@gmail.com>
2025-12-12 10:07:43 -0500

    Question 3 Initial commit

--- question2.js
+++ question2.js
@@ -6,3 +6,13 @@
 // 14: -14
 // -34: 34
 
+function opposite(number) {
+  return -number;
+}
+
+
+console.log(opposite(1));    // -1
+console.log(opposite(14));   // -14
+console.log(opposite(-34));  // 34
+console.log(opposite(0));    // 0
+console.log(opposite(-25));    // 25
